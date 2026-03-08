9c6a4d558f843f9270a6de896ae3267997e4268a
grl <857308047@qq.com>
2026-03-08 11:33:13 +0800

    refactor:moved TaskContext/context-switch into arch/riscv64 and cleaned a timer caller.

--- os/src/task/switch.rs
+++ os/src/task/switch.rs
@@ -1,9 +1,2 @@
-//!Wrap `switch.S` as a function
-use super::TaskContext;
-use core::arch::global_asm;
-
-global_asm!(include_str!("switch.S"));
-
-extern "C" {
-    pub fn __switch(current_task_cx_ptr: *mut TaskContext, next_task_cx_ptr: *const TaskContext);
-}
+//! Context switch re-export for the current arch.
+pub use crate::arch::__switch;
