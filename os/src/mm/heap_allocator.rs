9d10b111e82253c475c5ba653be422d7efb008e8
grl <857308047@qq.com>
2026-03-14 09:52:16 +0800

    feat:alloc_error详细日志

--- os/src/mm/heap_allocator.rs
+++ os/src/mm/heap_allocator.rs
@@ -9,6 +9,58 @@ static HEAP_ALLOCATOR: LockedHeap = LockedHeap::empty();
 #[alloc_error_handler]
 /// panic when heap allocation error occurs
 pub fn handle_alloc_error(layout: core::alloc::Layout) -> ! {
+    let proc_len = crate::task::pid2process_len();
+    let ready_len = crate::task::ready_queue_len();
+    let live_tasks = crate::task::live_task_count();
+    let (tracked_tasks, unique_task_pids, top_pid, top_pid_count, min_pid, max_pid) =
+        crate::task::live_task_pid_summary();
+    let timer_len = crate::timer::timer_len();
+    let (
+        map_len,
+        sampled_processes,
+        skipped_processes,
+        total_children,
+        total_tasks,
+        total_exited_threads,
+        max_children_pid,
+        max_children,
+        total_mutex_waiters,
+        total_sem_waiters,
+        total_cond_waiters,
+    ) = crate::task::pid2process_aggregate();
+    println!(
+        "[kernel] alloc_error diag: pid2pcb_len={} ready_queue_len={} timer_len={} live_tasks={}",
+        proc_len,
+        ready_len,
+        timer_len,
+        live_tasks
+    );
+    println!(
+        "[kernel] alloc_error diag2: map_len={} sampled={} skipped={} total_children={} total_tasks={} total_exited_threads={} max_children_pid={} max_children={}",
+        map_len,
+        sampled_processes,
+        skipped_processes,
+        total_children,
+        total_tasks,
+        total_exited_threads,
+        max_children_pid,
+        max_children
+    );
+    println!(
+        "[kernel] alloc_error diag3: mutex_waiters={} sem_waiters={} cond_waiters={}",
+        total_mutex_waiters,
+        total_sem_waiters,
+        total_cond_waiters
+    );
+    println!(
+        "[kernel] alloc_error diag4: tracked_tasks={} unique_task_pids={} top_pid={} top_pid_count={} min_pid={} max_pid={}",
+        tracked_tasks,
+        unique_task_pids,
+        top_pid,
+        top_pid_count,
+        min_pid,
+        max_pid
+    );
     panic!("Heap allocation error, layout = {:?}", layout);
 }
 /// heap space ([u8; KERNEL_HEAP_SIZE])
